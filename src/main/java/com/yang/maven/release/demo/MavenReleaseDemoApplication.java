6793ff33e213cc2fb37add237458e700299ee392
arther606 <>
2023-01-06 11:48:54 +0800

    init

---
+++ src/main/java/com/yang/maven/release/demo/MavenReleaseDemoApplication.java
@@ -0,0 +1,16 @@
+package com.yang.maven.release.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class MavenReleaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenReleaseDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yang/maven/release/demo/service/UserInfoService.java
@@ -0,0 +1,15 @@
+package com.yang.maven.release.demo.service;
+
+import com.yang.maven.release.demo.model.User;
+
+import java.util.List;
+
+/**
+ * @author yangliangchuang 2022/11/11 14:28
+ */
+
+public interface UserInfoService {
+
+    List<User> getUserInfoList();
+
+}
